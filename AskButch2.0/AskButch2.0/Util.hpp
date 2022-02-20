eaff5f77db159f6f41e6208f1322c8328c13b0e2
michalled <81343810+michalled@users.noreply.github.com>
2022-02-20 00:55:41 -0800

    Add files via upload

---
+++ AskButch2.0/AskButch2.0/Util.hpp
@@ -0,0 +1,22 @@
+#pragma once
+//
+//#include <iostream>
+//#include <SFML/Graphics.hpp>
+//
+//sf::Vector2f V2itoV2f(sf::Vector2i v);
+//sf::Vector2f V2utoV2f(sf::Vector2u v);
+//sf::Vector2f mouseInWorld(const sf::Transform& transform, const sf::RenderWindow& window);
+//
+//
+//float distance(const sf::Vector2f& v1, const sf::Vector2f& v2);
+//float distance(const sf::Vector2i& v1, const sf::Vector2i& v2);
+//
+//
+//bool touching(const sf::CircleShape& circle, const sf::Vector2f& position);
+//bool touching(const sf::CircleShape& circle, const sf::Vector2i& position);
+//bool touching(const sf::RectangleShape& rect, const sf::Vector2f& position);
+//bool touching(const sf::RectangleShape& rect, const sf::Vector2i& position);
+//
+//void displayButtons(sf::Transform t);
+//
+//std::vector<std::pair<ShapeButton<sf::RectangleShape>, std::string>> buttons;
